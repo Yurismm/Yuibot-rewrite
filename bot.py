d2cbc13c2ed9a8e0c8d36ea0ae78bd4a7f2da538
Yurismm <zizimorayo@gmail.com>
2018-07-23 00:40:46 +0100

    dsf
    dfsfdsf

--- bot.py
+++ bot.py
@@ -120,14 +120,6 @@ async def presence(ctx, Type=None, *, thing=None):
       else:
         await ctx.send('Usage: *presence [game/stream] [msg]')
 
-@bot.command(pass_context=True)
-async def invite(context):
-	invite = await client.create_invite(context.message.server,max_uses=1,xkcd=True)
-	await client.send_message(context.message.author,"Your invite URL is {}".format(invite.url))
-	await client.say ("Check Your Dm's :wink: ")
-
-
-
 
 if __name__ == "__main__":
     for extension in startup_extensions:
