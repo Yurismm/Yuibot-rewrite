ee4ee92a566886070a29e3893ea879c09a7d4ea8
Yurismm <zizimorayo@gmail.com>
2018-08-01 21:08:04 +0100

    adding on overwatch commands
    title

--- cogs/overwatch.py
+++ cogs/overwatch.py
@@ -1,8 +1,24 @@
 import discord
 import asyncio
-import overwatch.stats
+import discord
 from discord.ext import commands
+import overwatch.stats
+
+class Overwatch:
+    '''
+    Overwatch commands
+    '''
+
 
 
 @commands.command()
-#started commands when im back i will push my code here, DO NOT ADD THIS COG!!!!
+async def overwatchstats(self,ctx, user: str = None, stats):
+
+    stats = overwatch.stats.query('pc', user)
+    
+    if user == None:
+        await ctx.send("You need to use a blizzard username including its tag to search overwatch stats"
+
+    await ctx.send("'''"stats"'''")
+
+
