aebfa26788b986659bb065a9c3a440bf9b1209cf
Azizah Cherie Blackwood <40805624+Yurismm@users.noreply.github.com>
2018-08-01 13:41:35 +0100

    please PLEASE leave the status of the bot, on IDLE...
    PLEASE DON'T TOUCH THE STATUS BECAUSE I AM WORKING ON A WATCHING/LISTENING COMMAND... PLEASE

--- bot.py
+++ bot.py
@@ -120,7 +120,7 @@ async def on_ready():
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(status=discord.Status.online)
+    await bot.change_presence(status=discord.Status.idle)
 
 @bot.command()
 @is_dev()
