4641863676afaf1f7acbf27e5f6218d199d3c9e6
Yurismm <zizimorayo@gmail.com>
2018-08-10 04:03:33 +0100

    .
    .

--- bot.py
+++ bot.py
@@ -140,7 +140,7 @@ async def presence(ctx, Type=None, *, thing=None):
 
 
 @bot.command()
-async def say(self,*, content):
+async def say(ctx,self,*, content):
     await ctx.send(content)
                               
 
