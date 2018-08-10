1e93148ec2bb1a36546a80afd6547681cd6f2f3b
Yurismm <zizimorayo@gmail.com>
2018-08-10 04:21:33 +0100

    s
    d

--- bot.py
+++ bot.py
@@ -140,7 +140,7 @@ async def presence(ctx, Type=None, *, thing=None):
 
 
 @bot.command()
-async def say(ctx,*args,self,content):
+async def say(ctx,*args,content):
     content = ' '.join(args)
     await bot.delete_message(ctx.message)
     return await ctx.send(content)
