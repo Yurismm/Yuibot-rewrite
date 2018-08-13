99f5654e9ab4903228e7817b3051c2bb7c40b133
Azizah Cherie Blackwood <40805624+Yurismm@users.noreply.github.com>
2018-08-14 00:58:28 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -144,6 +144,15 @@ async def say(ctx,*args):
     content = ' '.join(args)
     await ctx.send(content)
 
+@bot.command()
+async def cat(ctx,src):
+    src="http://thecatapi.com/api/images/get?format=src&type=gif"
+    embed = discord.Embed(color = 0xf76ce4)
+    embed.set_author(name = "A cat...Nothing serious")
+    embed.set_image(url = src)
+    embed.set_footer(text="Cat why not")
+    await ctx.send (embed = embed)
+
 
                               
 
