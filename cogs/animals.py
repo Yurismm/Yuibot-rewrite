ff8ba9fd3bcb20a96ea1f6d64e199efb36e3fdd7
James Guess <jamesguessis@gmail.com>
2018-08-17 10:23:24 -0400

    Update animals.py

--- cogs/animals.py
+++ cogs/animals.py
@@ -1,31 +1,21 @@
 import discord
 from discord.ext import commands
 
-class animals():
+
+class Animals():
     def __init__(self, bot):
         self.bot = bot
 
 
 @commands.command()
-async def cat(ctx,src,self):
-    src="http://thecatapi.com/api/images/get?format=src&type=gif"
-    embed = discord.Embed(color = 0xf76ce4)
-    embed.set_author(name = "A cat...Nothing serious")
-    embed.set_image(url = src)
+async def cat(ctx, src, self):
+    src = "http://thecatapi.com/api/images/get?format=src&type=gif"
+    embed = discord.Embed(color=0xf76ce4)
+    embed.set_author(name="A cat...Nothing serious")
+    embed.set_image(url=src)
     embed.set_footer(text="Cat why not")
-    await ctx.send (embed = embed)
+    await ctx.send(embed=embed)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+def setup(bot):
+    bot.add_cog(Animals(bot))
