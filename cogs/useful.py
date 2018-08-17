c09aae824cfcf0b58addd43253e4d34356f89839
James Guess <jamesguessis@gmail.com>
2018-08-17 10:15:53 -0400

    Update useful.py

--- cogs/useful.py
+++ cogs/useful.py
@@ -1,10 +1,9 @@
 import discord
 import asyncio
 from discord.ext import commands
-import *
 
 
-class useful:
+class Useful:
     '''
     Useful commands
     '''
@@ -14,15 +13,13 @@ class useful:
 
 
 @commands.command()
-async def say(ctx, saymsg : str = None):
+async def say(ctx, saymsg: str = None):
     """Makes me say something"""
-    if saymsg == None:
-        return await ctx.send('Please provide something for the bot to say.')
-        
-    else: 
+    if not saymsg:
+        await ctx.send('Please provide something for the bot to say.')
+    else:
         await ctx.send(saymsg)
 
 
-    
-
-
+def setup(bot):
+    bot.add_cog(Useful(bot))
