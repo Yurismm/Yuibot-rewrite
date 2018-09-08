9414a4e085aa7a349ee35a4586c1fb66f99c4ceb
Aron B <25285064+MrArobuz@users.noreply.github.com>
2018-09-09 00:36:54 +0200

    Update sudo.py

--- cogs/sudo.py
+++ cogs/sudo.py
@@ -23,6 +23,26 @@ class sudo:
         role = discord.utils.get(ctx.guild.roles, name=role_name)
         user = ctx.message.author
         await user.add_roles(role)
+        
+
+    @commands.command(name='ban')
+    @is_dev()
+    async def ban(self, ctx, user:discord.Member, *, reason:str=None):
+        """Bans the specified user from the server"""
+        if reason is None:
+            reason = Language.get("moderation.no_reason", ctx)
+        reason += Language.get("moderation.banned_by", ctx).format(ctx.author)
+        try:
+            await ctx.guild.ban(user, delete_message_days=0, reason=reason)
+        except discord.errors.Forbidden:
+            if user.top_role.position == ctx.me.top_role.position:
+                await ctx.send(Language.get("moderation.no_ban_highest_role", ctx))
+            elif user.top_role.position > ctx.me.top_role.position:
+                await ctx.send(Language.get("moderation.no_ban_higher_role", ctx))
+            else:
+                await ctx.send(Language.get("moderation.no_ban_perms", ctx))
+            return
+        await ctx.send(Language.get("moderation.ban_success", ctx).format(user))
 
 
     @commands.command(name='fix')
